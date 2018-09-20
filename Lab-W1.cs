5bbd7bc9bb5df2503f7018baed63f3637d0d99a1
plasmasloth <plasmasloth44@gmail.com>
2018-09-19 20:31:40 -0400

    Add files via upload

---
+++ Lab-W1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myName = "Jack Jester";
+            string message = "Hello World!";
+
+            Console.WriteLine( message + " from " + myName);
+            Console.ReadLine();
+
+        }
+    }
+}
